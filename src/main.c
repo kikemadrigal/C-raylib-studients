05f0bfabea6d4d2e8ed7ded8846b2fc93bb8dde5
kikemadrigal <kikemadrigal@hotmail.com>
2025-01-12 23:12:16 +0100

    update game.html

--- src/main.c
+++ src/main.c
@@ -6,12 +6,11 @@ Game game;
 
 int main(void)
 {
-    const int screenWidth = 450;
-    const int screenHeight = 800;
+    const int screenWidth = 800;
+    const int screenHeight = 600;
     InitWindow(screenWidth, screenHeight, "Students");
     SetTargetFPS(60);  
     init_game(&game);
-    SetWindowSize(450, 800);
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         update_game(&game);
